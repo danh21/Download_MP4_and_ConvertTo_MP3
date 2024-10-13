499cfff0a62a874ff3c2bb90f18557a46d134975
Danh Phan <phandanh2112@gmail.com>
2024-10-13 17:23:41 +0700

    v1.1.0: add argument url of playlist in CLI, update readme

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Download YouTube videos and convert them to audio files (.mp3 files)
 # Requirement
 - Internet connection
 - Install python
-- pip install pytubefix
+- Install pytubefix
+- YouTube playlist must be PUBLIC
 
 # How to use
-- In **main.py**, config **playlist_url** variable to your YouTube playlist (playlist must be PUBLIC)
-- Open CMD at folder of this project, run command as below:\
-***python main.py***
-- Audio files are saved in folder **playlist** defaultly (custom config in **main.py**)
+- Open CMD at folder of this project, run command as below to know how to use this tool:\
+***python main.py -h***
+- Audio files are saved in folder **playlist** defaultly (config in **main.py**)
 
 # Reference
 https://pytubefix.readthedocs.io/en/latest/
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+# Include
+import sys
 from pytubefix import Playlist
 
 
 # Config
-playlist_url = "https://www.youtube.com/playlist?list=PLnLNse3s5NSur0jeszOqIVMZkxC3kr_QA"
 playlist_path = "./playlist/"
 
 
-playlist = Playlist(playlist_url)
+# Main
+if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    print("""
+    How to run:
+        python main.py <url-of-your-playlist>
+    """)
+else:
+    playlist = Playlist(sys.argv[1])
 
-for video in playlist.videos:
-    print("Downloading " + video.title + " ...")
-    audio_stream = video.streams.get_audio_only()
-    audio_stream.download(output_path=playlist_path, mp3=True)
+    for video in playlist.videos:
+        print("Downloading " + video.title + " ...")
+        audio_stream = video.streams.get_audio_only()
+        audio_stream.download(output_path=playlist_path, mp3=True)
 
-print("Handle all audios successfully!")
+    print("\nHandle all audios successfully!")
